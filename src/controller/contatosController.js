49fef486314766b841efaa6f18ddbe76d2a07c24
camilabulcao <camilabulcao5@gmail.com>
2020-11-13 15:56:55 -0300

    criando o delete

--- src/controller/contatosController.js
+++ src/controller/contatosController.js
@@ -1,13 +1,13 @@
 const { response } = require("express")
 const contatoCollections = require("../models/contatoSchema")
 
-const getAll = (request, response)=>{
+const getAll = (request, response) => {
     console.log(request.url)
 
-    contatoCollections.find((error, contatos) =>{
-        if(error){
+    contatoCollections.find((error, contatos) => {
+        if (error) {
             return response.status(500).send(error)
-        } else{
+        } else {
             return response.status(200).send({
                 mensagem: "GET com sucesso",
                 contatos
@@ -16,14 +16,14 @@ const getAll = (request, response)=>{
     })
 }
 
-const addContato = (request, response)=>{
+const addContato = (request, response) => {
     const contatoDoBody = request.body //pegando o body que o usuario digitou
     const contato = new contatoCollection(contatoDoBody)//criando um novo dado com o body
 
-    contato.save((error)=>{
-        if(error){
+    contato.save((error) => {
+        if (error) {
             return response.status(400).send(error)
-        }else{
+        } else {
             return response.status(200).send({
                 mensagem: "POST com sucesso",
                 contato
@@ -32,15 +32,15 @@ const addContato = (request, response)=>{
     })
 }
 //getContatoID
-const getContatoID = (request, response) =>{
+const getContatoID = (request, response) => {
     const idParams = request.params.id
-    contatoCollections.findById(idParams, (error, contatoID)=>{
-        if(error){
+    contatoCollections.findById(idParams, (error, contatoID) => {
+        if (error) {
             return response.status(500).send(error)
-        }else{
-            if(contatoID){
+        } else {
+            if (contatoID) {
                 return response.status(200).send(contatoID)
-            }else{
+            } else {
                 return response.status(404).send("Pessoa não encontrada")
             }
         }
@@ -49,13 +49,38 @@ const getContatoID = (request, response) =>{
 }
 
 //getByNome
-const getByNome = (request, response) =>{
-    const nomeParms = request.params.nome
-    contatoCollection.find({})
-}
+const getByNome = (request, response) => { 
+    const nomeParms = request.params.nome 
+    console.log(nomeParms)
+    contatoCollections.findOne({nome: nomeParms }, (error, contato) => 
+    {  if (error) { 
+        return response.status(500).send(error) 
+    } else if (contato) {
+         return response.status(400).json({ mensagem: 'Ops contato não encontrado!', }
+         ) } else {
+            return response.status(200).send(contato) 
+            } 
+        }) 
+    }
+
+//delete
+const deleteContato = (request, response) =>{
+    const idParams = request.params.id
+    contatoCollections.findByIdAndDelete(idParams, (error,contatoID)=>{
+        if(error){
+            return response.status(500).send(error)
+        }else if(contatoID){
+            return response.status(200).send({message: "Deletado com Sucesso"})
+        }else{
+            return response.status(404).send("Pessoa não encontrada")
+        }
+    })
+}    
 
 module.exports = {
     getAll,
     addContato,
-    getContatoID
+    getContatoID,
+    getByNome,
+    deleteContato
 }
--- src/routes/contatosRoute.js
+++ src/routes/contatosRoute.js
@@ -5,5 +5,7 @@ const controller = require("../controller/contatosController")
 router.get("/", controller.getAll)
 router.post("/criar", controller.addContato)
 router.get("/id/:id", controller.getContatoID)
+router.get("/nome/:nome", controller.getByNome)
+router.delete("/id/:id", controller.deleteContato)
 
 module.exports = router
